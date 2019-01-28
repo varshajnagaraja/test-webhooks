aacbd864071a1f0be7643872a7bd716076574787
varshajnagaraja <varsha.nagaraja@block.one>
2019-01-28 10:39:05 +0800

    Code change

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ var express = require('express');
 var app = express();
 const bodyParser = require("body-parser");
 
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
- */
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
 app.use(bodyParser.json());
 
  
